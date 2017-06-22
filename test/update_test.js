8982e55ee2aa145c335e22ed6bd0e04f88a90553
Thomas Beetz <t1h1az@Thomass-MBP.local>
2017-06-22 22:48:18 +0200

    upsate model instance test added and working

--- test/update_test.js
+++ test/update_test.js
@@ -23,4 +23,13 @@ describe('Updating database', () => {
       });
   });
 
+  it('model instance update', (done) => {
+    joe.update({ name: 'Sue'})
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        done();
+      });
+  });
+
 });
