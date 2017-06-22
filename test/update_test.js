a1b9cff7153ab6d50f1d2383bc3b57fe978bc235
Thomas Beetz <t1h1az@Thomass-MBP.local>
2017-06-22 23:50:29 +0200

    update test working class and instance based

--- test/update_test.js
+++ test/update_test.js
@@ -5,31 +5,49 @@ describe('Updating database', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({
-      name: 'joe'
-    });
+    joe = new User({name: 'joe'});
     joe.save()
       .then(() => done());
   });
 
-  it('model instance set n save', (done) => {
-    joe.set('name', 'Sue');
-    joe.save()
+  function assertName(operation, done) {
+    operation
       .then(() => User.find({}))
       .then((users) => {
         assert(users.length === 1);
         assert(users[0].name === 'Sue');
         done();
       });
+  }
+
+  it('model instance set n save', (done) => {
+    joe.set('name', 'Sue');
+    assertName(joe.save(), done);
   });
+
 
   it('model instance update', (done) => {
-    joe.update({ name: 'Sue'})
-      .then(() => User.find({}))
-      .then((users) => {
-        assert(users.length === 1);
-        done();
-      });
+    assertName(joe.update({ name: 'Sue' }), done);
   });
 
+  it('xa modal class can update', (done) => {
+    assertName(
+      User.update({name: 'joe'}, {name : 'Sue'}),
+      done
+    );
+  });
+
+  it(' a modal class can find one and update', (done) => {
+    assertName(
+      User.findOneAndUpdate({name: 'joe'}, {name : 'Sue'}),
+      done
+    );
+  });
+
+  it(' a modal class can find one by id and update', (done) => {
+    assertName(
+      User.findByIdAndUpdate(joe._id, {name : 'Sue'}),
+      done
+    );
+  });
 });
